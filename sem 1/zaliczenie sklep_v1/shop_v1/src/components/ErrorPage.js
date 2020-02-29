c164c9d3468bcd84ef99da07543b90ada4420c2b
Daniel Majewski <56756987+MajewskiDaniel@users.noreply.github.com>
2020-02-29 17:34:56 +0100

    Update ErrorPage.js

--- sem 1/zaliczenie sklep_v1/shop_v1/src/components/ErrorPage.js
+++ sem 1/zaliczenie sklep_v1/shop_v1/src/components/ErrorPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/App.css";
 
 const ErrorPage = () => (
-  <h1 className="error">podana strona nie istnieje :-(</h1>
+  <h1 className="error">Podana strona nie istnieje :-(</h1>
 );
 
 export default ErrorPage;
